d2acac16be25455749961cded30bc7235a87b893
Vikas18597 <khativikas97@gmail.com>
2023-03-27 14:09:44 +0530

    Using webpack to create the first template

--- src/home.js
+++ src/home.js
@@ -6,8 +6,18 @@ const home = () => {
     let str = "Welcome to The Khati's <br> \
     We provide authentic Indian Food <br> \
     Serving happiness Since 1998"
-    let addDiv = addComp("contents", "div", str, "home-intro")
-    addDiv.addElem()
+    addComp("contents", "div", "", "home-page").addElem()
+    addComp("home-page", "div", str, "home-intro").addElem()
+    addComp("home-page", "div", "", "pics").addElem()
+    
+    addComp("pics", "img", "", "pic1").addElem()
+    document.getElementById("pic1").setAttribute( 'src' , '../src/images/food_pic_1.jpg')
+    
+    addComp("pics", "img", "", "pic2").addElem()
+    document.getElementById("pic2").setAttribute( 'src' , '../src/images/food_pic_2.jpg')
+    
+    addComp("pics", "img", "", "pic3").addElem()
+    document.getElementById("pic3").setAttribute( 'src' , '../src/images/food_pic_3.jpg')
 }
 
 
